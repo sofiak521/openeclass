7fb060a2371b8f03789cc2832e91f672bb09689f
sofiak521 <kati@inf.uth.gr>
2014-11-02 19:52:40 +0200

    exercise3

---
+++ config/config.php
@@ -0,0 +1,10 @@
+<?php
+/* ========================================================
+ * Open eClass 3.0 configuration file
+ * Created by install on 2014-10-21 22:29
+ * ======================================================== */
+
+$mysqlServer = 'localhost';
+$mysqlUser = 'root';
+$mysqlPassword = 'aeras111';
+$mysqlMainDb = 'eclass';
